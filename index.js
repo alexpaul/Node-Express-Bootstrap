2688ff16ff76f01908c01bdc23035bd74fbd6ffa
alexpaul <alexpaulnyc@gmail.com>
2021-11-02 19:33:33 -0400

    added home route comment.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.listen(port, () => {
     console.log(`Node Server running on Port ${port}`)
 })
 
+// run `node index.js` and visit localhost:3000 to view rendered HTML page
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to our Homepage.</h1>`)
 })
